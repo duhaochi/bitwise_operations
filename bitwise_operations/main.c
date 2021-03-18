ae16fd722aefeeb98979c8f2d0d1dcc599318c9e
duhaochi <haochidu@my.uri.edu>
2021-03-18 07:54:36 -0400

    Initial Commit

---
+++ bitwise_operations/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  bitwise_operations
+//
+//  Created by duhaochi on 3/18/21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
