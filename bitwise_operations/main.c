7cd3721109395ed778ce148a0539ef89b5b2a48c
duhaochi <haochidu@my.uri.edu>
2021-03-18 08:07:05 -0400

    practical uses of bitwise operations

--- bitwise_operations/main.c
+++ bitwise_operations/main.c
@@ -6,9 +6,35 @@
 //
 
 #include <stdio.h>
+void onoff(void);
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    printf("Hello, World!\n");
+    onoff();
     return 0;
+}
+
+//practical uses of bitwise operation
+
+void onoff(){
+    
+    unsigned int flags;
+    flags = 1;
+    
+    
+    
+    //notice bellow we are using 8 bytes of memory for just a boolean value. which only takes up 1 bit. lets say we now have n switches we would need n * 4 byte space to store all of them! what a waste of space
+    int x, y;
+    x = 1;
+    y = 1;
+    //---------------
+    
+    
+    if ((flags & 1) != 0){
+        printf("1st feeature is enabled\n");
+    }
+    
+    if ((flags & 2) != 0){
+        printf("2nd feature is enabled\n");
+    }
 }
